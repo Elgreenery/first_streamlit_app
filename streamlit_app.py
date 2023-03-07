fce193ec5f93f43dca485a5f8fb36dd319322893
Elgreenery <127103349+Elgreenery@users.noreply.github.com>
2023-03-07 11:45:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,10 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
