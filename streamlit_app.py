1901238a3428a7b1bece9ba863c9237816b93a76
Elgreenery <127103349+Elgreenery@users.noreply.github.com>
2023-03-07 11:46:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,3 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit list contains:")
 streamlit.text(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
